709e5a1e30cd3452e541b1dd1b2f905c5bc4bd24
Kuppam Hyndhavi <153989010+kuppamhyndhavi@users.noreply.github.com>
2025-03-06 00:02:12 +0530

    Add files via upload

---
+++ Janmansh and Assignments.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int X;
+	    cin>>X;
+	    if(X+3<=10){
+	        cout<<"YES"<<endl;
+	    }
+	    else{
+	        cout<<"NO"<<endl;
+	    }
+	}
+	return 0;
+
+}
