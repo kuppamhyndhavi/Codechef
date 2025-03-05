84bafd22cd03b2f89a1cad99a93f751a356ee24c
Kuppam Hyndhavi <153989010+kuppamhyndhavi@users.noreply.github.com>
2025-03-05 19:09:40 +0530

    Add files via upload

---
+++ Subscriptions.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int N,X;
+	    cin>>N>>X;
+	    int sub=(N+5)/6;
+	    int total=sub*X;
+	    cout<<total<<endl;
+	}
+	return 0;
+
+}
