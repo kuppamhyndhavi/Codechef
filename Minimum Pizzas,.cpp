44fde986a4b8a44f8173238d87a1c3a25e6e4d4b
Kuppam Hyndhavi <153989010+kuppamhyndhavi@users.noreply.github.com>
2025-03-09 10:19:35 +0530

    Add files via upload

---
+++ Minimum Pizzas,.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,x;
+	    cin>>n>>x;
+	    int ts=n*x;
+	    int pn=(ts+3)/4;
+	    cout<<pn<<endl;
+	}
+	return 0;
+
+}
