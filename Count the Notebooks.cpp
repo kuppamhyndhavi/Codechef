e0c0dddc507aaa1ffbaac4f96212f314040b0c1c
Kuppam Hyndhavi <153989010+kuppamhyndhavi@users.noreply.github.com>
2025-03-17 20:22:53 +0530

    Add files via upload

---
+++ Count the Notebooks.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    int tp=n*1000;
+	    int nb=tp/100;
+	    
+	    cout<<nb<<endl;
+	}
+	return 0;
+
+}
