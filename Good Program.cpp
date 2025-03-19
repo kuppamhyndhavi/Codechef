a65c5a38e5078744e5e974fcb11667c1a3c23416
Kuppam Hyndhavi <153989010+kuppamhyndhavi@users.noreply.github.com>
2025-03-19 20:51:14 +0530

    Add files via upload

---
+++ Good Program.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    
+	
+	int n;
+	cin>>n;
+	if(n%4==0){
+	    cout<<"good"<<endl;
+	}
+	else{
+	    cout<<"not good"<<endl;
+	}
+	}
+	
+
+	return 0;
+
+}
+Good Program
