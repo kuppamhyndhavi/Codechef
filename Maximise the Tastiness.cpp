00e30738a7e5dc82e114b09848509f1343c00aac
Kuppam Hyndhavi <153989010+kuppamhyndhavi@users.noreply.github.com>
2025-03-21 11:05:19 +0530

    Add files via upload

---
+++ Maximise the Tastiness.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int a,b,c,d;
+	    cin>>a>>b>>c>>d;
+	    int ts=max(a,b )+ max(c,d);
+	    cout<<ts<<endl;
+	    }
+	    return 0;
+    
+
+
+}
