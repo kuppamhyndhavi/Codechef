81c8d3ada291f87e2d20f0f29416ea1e8f828197
Kuppam Hyndhavi <153989010+kuppamhyndhavi@users.noreply.github.com>
2025-03-21 11:05:46 +0530

    Add files via upload

---
+++ Police and Thief.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,y;
+	cin>>x>>y;
+	int dis=abs(x-y);
+	int time=dis;
+	
+	
+	cout<<time<<endl;
+}
+
+	
+	//cout<<time<<endl;
+
+
+
+	return 0;
+	
+}
