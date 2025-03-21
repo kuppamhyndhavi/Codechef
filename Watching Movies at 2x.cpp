72d639a29aa18f622286902fafe264fb97b2520b
Kuppam Hyndhavi <153989010+kuppamhyndhavi@users.noreply.github.com>
2025-03-21 11:05:33 +0530

    Add files via upload

---
+++ Watching Movies at 2x.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x,y;
+	cin>>x>>y;
+	int t1=y/2;
+	int t2=x-y;
+	int tt=t1+t2;
+	cout<<tt<<endl;
+	return 0;
+
+}
