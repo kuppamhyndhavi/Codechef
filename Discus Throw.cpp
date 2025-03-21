f353a1b3dc5235f3c376921d78dd67bed4bc48de
Kuppam Hyndhavi <153989010+kuppamhyndhavi@users.noreply.github.com>
2025-03-21 11:05:06 +0530

    Add files via upload

---
+++ Discus Throw.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int a,b,c;
+	    cin>>a>>b>>c;
+	    int maxd=max({a,b,c});
+	        cout<<maxd<<endl;
+	        
+	    
+	}
+	return 0;
+
+}
