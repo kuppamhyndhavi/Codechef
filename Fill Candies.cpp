58f3d62c0abaa23c3a91d332aa14e6efc9cd289d
Kuppam Hyndhavi <153989010+kuppamhyndhavi@users.noreply.github.com>
2025-03-24 18:56:44 +0530

    Create Fill Candies.cpp

---
+++ Fill Candies.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,k,m;
+	    cin>>n>>k>>m;
+	    int cap=k*m;
+	    int bag=(n+cap-1)/cap;
+	    cout<<bag<<endl;
+	    
+	}
+
+}
