afb970bfee9bb876fea345e78af0495d658fd192
Kuppam Hyndhavi <153989010+kuppamhyndhavi@users.noreply.github.com>
2025-03-25 09:18:37 +0530

    Create Chef and his Apps.cpp

---
+++ Chef and his Apps.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int s,x,y,z;
+	    cin>>s>>x>>y>>z;
+	    int uu=s-(x+y);
+	    if(uu>=z){
+	        cout<<"0"<<endl;
+	    }
+	    else if((s-x)>=z||(s-y)>z){
+	        cout<<  "1"<<endl;
+	    }
+	    else{
+	        cout<<"2"<<endl;
+	    }
+	    
+	}
+
+}
