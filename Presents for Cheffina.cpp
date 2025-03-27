3d9c011cfcf43e5bd492d8bd488c027d25e3504a
Kuppam Hyndhavi <153989010+kuppamhyndhavi@users.noreply.github.com>
2025-03-27 18:22:08 +0530

    Create Presents for Cheffina.cpp

---
+++ Presents for Cheffina.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    int tg=n-(n/5);
+	    cout<<tg<<endl;
+	}
+
+}
