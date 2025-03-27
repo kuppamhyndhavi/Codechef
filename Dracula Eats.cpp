d48fac9579e4ecbefb891e0241c5a03632978915
Kuppam Hyndhavi <153989010+kuppamhyndhavi@users.noreply.github.com>
2025-03-27 18:22:51 +0530

    Create Dracula Eats.cpp

---
+++ Dracula Eats.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    int tue=n/7;
+	    if(n%7>=2)
+	        tue++;
+	        cout<<tue<<endl;
+	    }
+	
+
+}
