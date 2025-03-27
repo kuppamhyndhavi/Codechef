41e4dd5d2df21c6d1dfd682ba062f12281857f61
Kuppam Hyndhavi <153989010+kuppamhyndhavi@users.noreply.github.com>
2025-03-27 18:20:21 +0530

    Create Too many items.cpp

---
+++ Too many items.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    int pb=(n+9)/10;
+	    cout<<pb<<endl;
+	    
+	    
+	}
+
+}
