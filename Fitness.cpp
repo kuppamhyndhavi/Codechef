f7646ed2b69ea7f5186b2e0b53d3ec3bba8ca23c
Kuppam Hyndhavi <153989010+kuppamhyndhavi@users.noreply.github.com>
2025-03-30 12:16:37 +0530

    Create Fitness.cpp

---
+++ Fitness.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int x;
+        cin>>x;
+        // your code goes here
+        int tw=2*x*5;
+        cout<<tw<<endl;
+        
+    }
+}
