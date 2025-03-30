56f1817685123b6fb97fb2f360227c2b163bfb79
Kuppam Hyndhavi <153989010+kuppamhyndhavi@users.noreply.github.com>
2025-03-30 12:20:30 +0530

    Create Chef and Chocolates.cpp

---
+++ Chef and Chocolates.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int x,y,z;
+        cin>>x>>y>>z;
+        int tt=(5*x+10*y);
+        int ttt=tt/z;
+        cout<<ttt<<endl;
+        // your code goes here
+    }
+	
+
+}
