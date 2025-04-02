0ed36dec4b51f6368236e91ff4b9d99996c63bce
Kuppam Hyndhavi <153989010+kuppamhyndhavi@users.noreply.github.com>
2025-04-02 18:27:19 +0530

    Create Favourite Numbers.cpp

---
+++ Favourite Numbers.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int a;
+        cin >> a;
+        // your code goes here
+        if (a % 2 == 0 && a % 7 == 0) {
+            cout << "ALICE" << endl;
+        }
+        else if (a % 2 != 0 && a % 9 == 0) {
+            cout << "BOB" << endl;
+        }
+        else {
+            cout << "CHARLIE" << endl;
+        }
+    }
+
+}
